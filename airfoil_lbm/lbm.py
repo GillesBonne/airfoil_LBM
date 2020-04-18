40c6efb472f7349d254094a2e7fb1f37ad4d556d
mruijzendaal <martijn.ruijzendaal@gmail.com>
2020-04-18 11:15:59 +0200

    Numba'ed some LBM functions

--- airfoil_lbm/lbm.py
+++ airfoil_lbm/lbm.py
@@ -4,7 +4,7 @@ import numba
 import numpy as np
 
 
-# @numba.jit(nopython=True, cache=True)
+@numba.jit(nopython=True, cache=True)
 def calculate_macros(f, ex, ey) -> typing.Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Calculates the macroscopic variables (density, velocity_x, velocity_y) for the given field.
@@ -14,25 +14,24 @@ def calculate_macros(f, ex, ey) -> typing.Tuple[np.ndarray, np.ndarray, np.ndarr
     """
     # Calculate rho. This is the sum over the contents of each q (velocity vector)
     rho = f.sum(axis=0)
-
-    e = np.array((ex, ey)).T
-    u = np.dot(e.transpose(), f.transpose((1, 0, 2))) / rho
-    ux, uy = u
+    #
+    # u = np.dot(e, f.transpose((1, 0, 2))) / rho
+    # ux, uy = u
 
     # The velocity is the mass-averaged velocity for each direction
     # Ux is periodic if fp is also periodic, so we don't have to apply pbc
-    # ux = np.zeros(rho.shape)
-    # uy = np.zeros(rho.shape)
-    #
-    # exi = ex.reshape((ex.size, 1, 1))
-    # eyi = ey.reshape((ex.size, 1, 1))
-    #
-    # fx = (exi * f).sum(axis=0)
-    # fy = (eyi * f).sum(axis=0)
-    #
-    # for i, j in zip(*np.nonzero(rho)):
-    #     ux[i, j] = fx[i, j] / rho[i, j]
-    #     uy[i, j] = fy[i, j] / rho[i, j]
+    ux = np.zeros(rho.shape)
+    uy = np.zeros(rho.shape)
+
+    exi = ex.reshape((ex.size, 1, 1))
+    eyi = ey.reshape((ex.size, 1, 1))
+
+    fx = (exi * f).sum(axis=0)
+    fy = (eyi * f).sum(axis=0)
+
+    for i, j in zip(*np.nonzero(rho)):
+        ux[i, j] = fx[i, j] / rho[i, j]
+        uy[i, j] = fy[i, j] / rho[i, j]
 
     # Increment velocities in x-direction to mimic a constant pressure drop
     # ux[rho > 0] += 0.001 / rho[rho > 0]
